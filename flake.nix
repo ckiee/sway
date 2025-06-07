fd58a9e28488e52fbe30bde137a71b69d87f7480
Tris Emmy Wilson <tris@tris.fyi>
2025-06-07 14:23:12 -0400

    bump flake versions

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "sway wayland compositor";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
     wlroots-tris = {
       url = "github:an-empty-string/wlroots-hacks/tris-patches";
       inputs.nixpkgs.follows = "nixpkgs";
