c9b7edb1019a0623cfc5fb46899b8e15d964dfbb
ckie <git-525ff67@ckie.dev>
2026-02-06 10:33:49 +0200

    flake-compat

--- flake.nix
+++ flake.nix
@@ -6,15 +6,19 @@
       url = "github:an-empty-string/wlroots-hacks/tris-patches";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-compat = {
+      url = "github:ckiee/flake-compat/add-overrideInputs";
+      flake = false;
+    };
   };
-  outputs = { self, nixpkgs, wlroots-tris }: {
-    packages.x86_64-linux.default = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in (pkgs.sway-unwrapped.override {
+  outputs = { self, nixpkgs, wlroots-tris, ... }: {
+    packages.x86_64-linux.default =
+      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in (pkgs.sway-unwrapped.override {
         wlroots = wlroots-tris.packages.x86_64-linux.default;
       }).overrideAttrs (old: {
-      version = "${old.version}-tris-patches";
-      src = ./.;
-    });
+        version = "${old.version}-tris-patches";
+        src = ./.;
+      });
   };
 }
